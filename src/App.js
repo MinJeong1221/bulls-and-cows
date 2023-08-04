e9255175fd3752e319a03143df850bf72cb0747b
MinJeong1221 <mjeong1998@naver.com>
2023-08-04 21:22:18 +0900

    정답 맞추기, 기록 남기기, 다시 시작하기 개발

--- src/App.js
+++ src/App.js
@@ -6,29 +6,76 @@ function App() {
   // console.log(generaterRandomNumber());
   const [randomNumber, setRandomNumber] = useState(generaterRandomNumber);
   const [answer, setAnswer] = useState("");
+  const [logs, setLogs] = useState([]);
+  const [isSuccess, setIsSuccess] = useState(false);
 
   useEffect(() => {
     console.log(randomNumber);
   }, [randomNumber]);
 
   const handleAnswerChanged = (e) => {
-    console.log(e.target.value);
     setAnswer(e.target.value);
+  };
+  const handleSubmitClick = (e) => {
+    const answerArray = answer.split("").map((item) => Number(item));
+    const { strike, ball } = randomNumber.reduce(
+      (prev, cur, index) => {
+        if (answerArray[index] === cur) {
+          return { ...prev, strike: prev.strike + 1 };
+        }
+        if (answerArray.includes(cur)) {
+          return {
+            ...prev,
+            ball: prev.ball + 1,
+          };
+        }
+        return prev;
+      },
+      {
+        strike: 0,
+        ball: 0,
+      }
+    );
+    if (strike === 4) {
+      alert("정답입니다");
+      setLogs([...logs, `${answer} (축하합니다 정답입니다!)`]);
+      setIsSuccess(true);
+      return;
+    }
+    // console.log("strike :", strike, "ball :", ball);
+    setLogs([...logs, `${answer} (strike : ${strike}, ball : ${ball})`]);
+  };
+  const handleRetryClick = () => {
+    setRandomNumber(generaterRandomNumber());
+    setAnswer("");
+    setLogs([]);
+    setIsSuccess(false);
   };
 
   return (
     <div className="App">
       <h1>숫자 야구 게임</h1>
-      <header className="header">{randomNumber}</header>
+      <header className="header">
+        {isSuccess ? `정답은 : ${answer}` : "_ _ _ _"}
+      </header>
       <section>
-        <input type="text" value={answer} onChange={handleAnswerChanged} />
-        <button>맞춰보기</button>
+        <input
+          type="text"
+          value={answer}
+          onChange={handleAnswerChanged}
+          disabled={isSuccess}
+        />
+        {isSuccess ? (
+          <button onClick={handleRetryClick}>다시하기</button>
+        ) : (
+          <button onClick={handleSubmitClick}>맞춰보기</button>
+        )}
       </section>
       <h2>기록</h2>
       <ol>
-        <li>1234 (strike: 0, ball : 2)</li>
-        <li>1234 (strike: 0, ball : 2)</li>
-        <li>1234 (strike: 0, ball : 2)</li>
+        {logs.map((log, index) => {
+          return <li key={`${log}_${index}`}>{log}</li>;
+        })}
       </ol>
     </div>
   );
